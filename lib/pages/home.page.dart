0f7b21777b54275e64d2383796124e9da609e9d1
Guilherme Meneses Pereira <gmeneses@everis.com>
2019-10-28 12:20:02 -0300

    [add] Animation

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Color _color = Colors.deepPurple;
   var _gasCtrl = new MoneyMaskedTextController();
   var _alcCtrl = new MoneyMaskedTextController();
   var _busy = false;
@@ -20,21 +21,27 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          _completed
-              ? Success(
-                  reset: reset,
-                  result: _resultText,
-                )
-              : SubmitForm(
-                  alcCtrl: _alcCtrl,
-                  gasCtrl: _gasCtrl,
-                  submitFunc: calculate,
-                  busy: _busy,
-                ),
-        ],
+      body: AnimatedContainer(
+        duration: Duration(
+          milliseconds: 1200,
+        ),
+        color: _color,
+        child: ListView(
+          children: <Widget>[
+            Logo(),
+            _completed
+                ? Success(
+                    reset: reset,
+                    result: _resultText,
+                  )
+                : SubmitForm(
+                    alcCtrl: _alcCtrl,
+                    gasCtrl: _gasCtrl,
+                    submitFunc: calculate,
+                    busy: _busy,
+                  ),
+          ],
+        ),
       ),
     );
   }
@@ -51,31 +58,31 @@ class _HomePageState extends State<HomePage> {
     double res = alc / gas;
 
     setState(() {
+      _color = Colors.deepPurpleAccent;
       _completed = false;
       _busy = true;
     });
 
-    return new Future.delayed(
-        const Duration(seconds: 1),
-        (){
-              setState(() {
-                if (res >= 0.7) {
-                  _resultText = "Compensa utilizar Gasolina!";
-                } else {
-                  _resultText = "Compensa utilizar Álcool!";
-                }
-                _completed = true;
-                _busy = false;
-              });
-            });
+    return new Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        if (res >= 0.7) {
+          _resultText = "Compensa utilizar Gasolina!";
+        } else {
+          _resultText = "Compensa utilizar Álcool!";
+        }
+        _completed = true;
+        _busy = false;
+      });
+    });
   }
-  reset(){
+
+  reset() {
     setState(() {
       _alcCtrl = new MoneyMaskedTextController();
       _gasCtrl = new MoneyMaskedTextController();
       _completed = false;
-      _busy = false; 
+      _busy = false;
+      _color = Colors.deepPurple;
     });
   }
-
 }
